a0654d801e76a3a190478ff76c2c22623579ec73
Fergal McNamara <g00180082@atu.ie>
2025-09-28 16:30:53 +0100

    Initial Commit

---
+++ src/main/java/ie/atu/prelab2/PreLab2Application.java
@@ -0,0 +1,13 @@
+package ie.atu.prelab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreLab2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreLab2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/prelab2/PreLab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.prelab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreLab2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
